ad43686a225fe7268f85f1ed39ba05f6b2774bd4
Matt Ward <matt.ward@num.uk>
2019-03-28 11:56:04 +0000

    Add test class and simple parser class method
    Some crude code to get things going, tests a=1, a='1' to give a
    starting point for further development.
    
    Tests _will_ use the JSON test cases from the grammar project.

---
+++ src/main/python/modl.py
@@ -0,0 +1,48 @@
+import json
+from antlr4 import *
+from generated.MODLLexer import MODLLexer
+from generated.MODLParser import MODLParser
+from generated.MODLParserListener import MODLParserListener
+
+
+class JSONFormatter(MODLParserListener):
+    def __init__(self):
+        self.json_obj = {}
+
+    def to_json(self) -> str:
+        return json.dumps(self.json_obj)
+
+    def enterModl(self, ctx:MODLParser.ModlContext):
+        text = ctx.getText()
+        print(f"Entered MODL: {text}")
+
+    def enterModl_pair(self, ctx:MODLParser.Modl_pairContext):
+        self.name = str(ctx.STRING())
+
+    def enterModl_value(self, ctx:MODLParser.Modl_valueContext):
+        if ctx.NUMBER():
+            value = int(str(ctx.NUMBER()))
+        elif ctx.STRING():
+            value = str(ctx.STRING())
+        elif ctx.QUOTED():
+            value = str(ctx.QUOTED())[1:-1] # strip quotes
+        else:
+            value = None
+        self.json_obj = {self.name: value}
+
+    def exitModl(self, ctx:MODLParser.ModlContext):
+        print("MODL has left the building.")
+
+
+def to_json(input_stream) -> str:
+    # Wrap the input in an InputStream if it's just a string
+    if type(input_stream) == str:
+        input_stream = InputStream(input_stream)
+    lexer = MODLLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = MODLParser(stream)
+    tree = parser.modl()
+    walker = ParseTreeWalker()
+    listener = JSONFormatter()
+    walker.walk(listener, tree)
+    return listener.to_json()
