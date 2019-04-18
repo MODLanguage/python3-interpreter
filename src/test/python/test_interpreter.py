99cbffa12097ba0568ea885291e449f14d3f1726
Matt Ward <matt.ward@num.uk>
2019-04-18 14:59:40 +0100

    Tidy lookup value test (though still not working)

--- src/test/python/test_interpreter.py
+++ src/test/python/test_interpreter.py
@@ -1,26 +1,24 @@
 import unittest
+
 from modl import interpret
-from modl_creator import process_modl_parsed
-import parser
+from modl_creator import RawModlObject, Pair
 
 
 class MyTestCase(unittest.TestCase):
+    @unittest.skip('Not yet implemented')
     def test_object_index_as_array(self):
-        parsed = parser.parse('?=[red;green;blue]; fav=%1')
-        processed = process_modl_parsed(parsed)
-        interpreted = interpret(processed)
+        interpreted: RawModlObject = interpret('?=[red;green;blue]; fav=%1')
         # Test the resulting tree
-        pair = interpreted.structures[0].pair
-        self.assertEqual(('fav', 'green'), (pair.key, pair.value_item.value.get_value()))
+        pair:Pair = interpreted.get_by_index(0)
+        self.assertEqual(('fav', 'green'), (str(pair.key), pair.get_value()))
         self.assertEqual(1, len(interpreted.structures))
 
+    @unittest.skip('Not yet implemented')
     def test_object_index_as_multivalue(self):
-        parsed = parser.parse('?=red:green:blue; fav=%1')
-        processed = process_modl_parsed(parsed)
-        interpreted = interpret(processed)
+        interpreted: RawModlObject = interpret('?=red:green:blue; fav=%1')
         # Test the resulting tree
-        pair = interpreted.structures[0].pair
-        self.assertEqual(('fav', 'green'), (pair.key, pair.value_item.value.get_value()))
+        pair:Pair = interpreted.get_by_index(0)
+        self.assertEqual(('fav', 'green'), (str(pair.key), pair.get_value()))
         self.assertEqual(1, len(interpreted.structures))
 
 
